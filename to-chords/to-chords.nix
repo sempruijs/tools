d072a8ee99e7fb740eee7c3dfa8a6e575374bb9f
sem pruijs <sem@pruijs.net>
2024-04-28 17:34:34 +0200

    increase fontsize for to-chords

--- to-chords/to-chords.nix
+++ to-chords/to-chords.nix
@@ -1,6 +1,6 @@
 pkgs: pkgs.writeShellApplication {
   name = "audio-from-yt";
-  runtimeInputs = with pkgs; [ pandoc texliveTeTeX ];
+  runtimeInputs = with pkgs; [ pandoc texliveConTeXt ];
   text = ''
     printf "\n----- step 1 of 2 -----\n\n"
     printf "enter the file name (.md): "
@@ -10,7 +10,7 @@ pkgs: pkgs.writeShellApplication {
     printf "enter the output file name (.pdf): "
     read -r output_name
 
-    pandoc "$input_name" -o "$output_name"
+    pandoc "$input_name" --pdf-engine context -V fontsize=50pt -o "$output_name"
   '';
 }
 
