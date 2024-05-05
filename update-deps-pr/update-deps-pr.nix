d16f71e20caf7c445cfa4503124008fa8f3bacf6
sem pruijs <sem@pruijs.net>
2024-05-05 17:48:57 +0200

    add yes or no question

--- update-deps-pr/update-deps-pr.nix
+++ update-deps-pr/update-deps-pr.nix
@@ -4,6 +4,20 @@ pkgs: pkgs.writeShellApplication {
   runtimeInputs = with pkgs; [ gh coreutils git nix ];
 
   text = ''
+    function yes_or_no {
+      while true; do
+          read -r -p "$* [y/n]: " yn
+          case $yn in
+              [Yy]*) return 0  ;;  
+              [Nn]*) echo "Aborted" ; return  1 ;;
+          esac
+      done
+    }
+
+    printf "\n----- step 1 of 1 -----\n\n"
+    printf "this will update the nix flake lock file and opens a pr using github CLI.\n"
+    yes_or_no "continue?"
+
     if [ -z "$(git status --porcelain)" ]; then 
       DATE=$(date --iso-8601)
       echo "$DATE"
